506aeadc6c0c58bb028a97fddd494d948a0d88b3
rohan <rohankademani@outlook.in>
2020-08-15 13:33:02 +0530

    Updates

---
+++ precache-manifest.7b6aa1543ebdb8c2fb66b4334a358213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7f8d2cc6d52f7e00c92dab7560e43a",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "ff0f93b42b0a2a9e0c63",
+    "url": "/expense-tracker/static/css/main.986ea294.chunk.css"
+  },
+  {
+    "revision": "cccc01f328001ea43331",
+    "url": "/expense-tracker/static/js/2.7d3aaaac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.7d3aaaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0f93b42b0a2a9e0c63",
+    "url": "/expense-tracker/static/js/main.9ba99628.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
